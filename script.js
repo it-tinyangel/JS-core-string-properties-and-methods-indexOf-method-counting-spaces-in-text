9fb60dfc2f652b434569dfcc8c8cdf72720dfe1a
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-30 00:36:01 +0300

    Counting the spaces in the text

---
+++ script.js
@@ -0,0 +1,12 @@
+let userInputText = prompt(`Input your text:`);
+let spaceCount = 0;
+
+for (let i = 0; i < userInputText.length; i++) {
+
+	if (userInputText.indexOf(' ', i) !== -1) {
+		i = userInputText.indexOf(' ', i);
+		spaceCount++;
+	}
+}
+
+console.log(`The number of spaces in the inputted text: ${spaceCount}`);
